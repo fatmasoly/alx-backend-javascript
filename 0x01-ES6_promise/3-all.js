382a7fe0ccdac98ee1f7acb0809067bc9c911012
fatma soliman <fatmacivil@hotmail.com>
2024-06-26 11:34:07 +0300

    fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
-      console.log(values[0].body, values[1].firstName, values[1].lastName);
+      console.log('${result[0].body}, ${result[1].firstName}, ${result[1].lastName}');
     })
     .catch(() => console.log('Signup system offline'));
 }
