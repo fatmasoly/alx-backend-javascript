f00edb4d7867c48e63801f2549d9ad0c001485c0
fatma soliman <fatmacivil@hotmail.com>
2024-08-21 22:10:55 +0300

    fix payment test

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,11 +1,6 @@
 const Utils = require('./utils');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  if (typeof totalAmount !== 'number' || typeof totalShipping !== 'number') {
-    console.log('Invalid inputs');
-    return;
-  }
-
   const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${result}`);
 }
